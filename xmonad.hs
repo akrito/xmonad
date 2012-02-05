d13aaf2b6318f8431c7bc94ecaa739d6aa2fc271
Alex Kritikos <alex@8bitb.us>
2012-02-05 15:03:44 -0600

    Removed unused imports and commented code

--- xmonad.hs
+++ xmonad.hs
@@ -1,31 +1,28 @@
 {-# LANGUAGE FlexibleInstances, MultiParamTypeClasses, DeriveDataTypeable #-}
+import Control.Monad (msum)
+-- import Data.Bits ((.|.))
+-- import Data.Ratio
+-- import System.IO
 import XMonad
-import XMonad.Config.Gnome
-import XMonad.Config.Xfce
-import XMonad.Operations
-import XMonad.Hooks.ManageHelpers
-import XMonad.Hooks.FadeInactive
--- import XMonad.Hooks.ManageHelpers
-import XMonad.Layout.LayoutHints
-import qualified XMonad.StackSet as W
 import XMonad.Actions.CycleWS
-import XMonad.Layout
+import XMonad.Config.Gnome
+-- import XMonad.Config.Xfce
+-- import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.FadeInactive
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
+-- import XMonad.Layout
 import XMonad.Layout.BoringWindows
-import XMonad.Layout.Minimize
+import XMonad.Layout.LayoutHints
 import XMonad.Layout.NoBorders
 import XMonad.Layout.Simplest
 import XMonad.Layout.Spacing
-import XMonad.Hooks.ManageDocks
-import XMonad.Hooks.EwmhDesktops
---import XMonad.Hooks.RestoreMinimized
+-- import XMonad.Operations
 import qualified Data.Map as M
-import Data.Bits ((.|.))
-import Data.Ratio
-import System.IO
-import Control.Monad (msum)
---import XMonad.Hooks.SetWMName
+import qualified XMonad.StackSet as W
 
-layout' = smartBorders $ layoutHints $ avoidStruts $ boringAuto $ minimize $ spacing 2 $ fair ||| Simplest
+-- Layouts
+layout' = smartBorders $ layoutHints $ avoidStruts $ boringAuto $ spacing 2 $ fair ||| Simplest
     where
       fair = Fair delta ratio
       ratio = 1/2
@@ -36,40 +33,32 @@ layout' = smartBorders $ layoutHints $ avoidStruts $ boringAuto $ minimize $ spa
 data Fair a = Fair { fairRatio :: !Rational
                    , fairRatioIncrement :: !Rational }
                 deriving (Show, Read)
-
 instance LayoutClass Fair a where
     description _ = "Fair"
-
     pureLayout (Fair delta frac) r s = pureLayout (Tall nmaster delta frac) r s
       where
         n = length $ W.integrate s
         nmaster = floor $ fromIntegral n / 2
-
     pureMessage (Fair delta frac) m =
             msum [fmap resize     (fromMessage m)]
-
       where resize Shrink             = Fair delta (max 0 $ frac-delta)
             resize Expand             = Fair delta (min 1 $ frac+delta)
 
+-- Keybindings
 modMask' = mod4Mask
-
 defKeys    = keys defaultConfig
 delKeys x  = foldr M.delete           (defKeys x) (toRemove x)
 newKeys x  = foldr (uncurry M.insert) (delKeys x) (toAdd    x)
 toAdd x =
     [ ((modMask x, xK_s), sendMessage NextLayout)
     , ((modMask x, xK_w), kill)
-    -- These don't work in the Full layout
     , ((modMask x, xK_j), focusDown)
     , ((modMask x, xK_k), focusUp)
---    , ((modMask x, xK_m), withFocused (\f -> sendMessage (MinimizeWin f)))
---    , ((modMask x .|. shiftMask, xK_m), sendMessage RestoreNextMinimizedWin)
     ]
 toRemove XConfig{modMask = modm} =
     [ (modm              , xK_space ) ]
 
-myWorkspaces = map show [1..4]
-
+-- Mouse bindings
 myMouseBindings (XConfig {XMonad.modMask = modMask}) = M.fromList $
     -- mod-button1, Set the window to floating mode and move by dragging
     [ ((modMask', button1), (\w -> focus w >> mouseMoveWindow w >> windows W.shiftMaster))
@@ -89,41 +78,8 @@ myLogHook :: X ()
 myLogHook = fadeInactiveLogHook fadeAmount
     where fadeAmount = 0x99999999
 
-
--- main = do
---   xmonad conf
---        { startupHook = startupHook conf >> setWMName "LG3D"
---        }
-
--- gnomeManageHook = composeAll (
---     [ manageHook gnomeConfig
---     , className =? "Unity-2d-panel" --> doIgnore
---     , className =? "Unity-2d-launcher" --> doFloat
---     ])
-
+-- Gnome-specific
 gnomeManageHook = composeAll ([manageHook gnomeConfig, isFullscreen --> doFullFloat])
-
--- conf = ewmh defaultConfig
---                      { layoutHook         = layout'
---                      -- #adff2f is yellow-green
---                      -- #a4c98b is neutral green
---                      -- #147427 is darker green
---                      -- #ea551c is burnt orange
---                      -- #4698F0 is blue
---                      , normalBorderColor  = "#888888"
---                      , focusedBorderColor = "#4698F0"
---                      , modMask            = modMask'
---                      , borderWidth        = 2
---                      , keys              = newKeys
---                      , mouseBindings     = myMouseBindings
---                      , workspaces        = myWorkspaces
-                                           
---                      , manageHook = composeAll [
---                          isFullscreen --> doFullFloat
---                          ] <+> manageDocks
---                      , handleEventHook = restoreMinimizedEventHook
---                      }
-
 main = xmonad gnomeConfig
        { manageHook    = gnomeManageHook
        , modMask       = modMask'
@@ -133,12 +89,3 @@ main = xmonad gnomeConfig
        , logHook       = myLogHook
        , borderWidth   = 0
        }
-
--- main = xmonad xfceConfig
---        { modMask = modMask'
---        , keys = newKeys
---        , mouseBindings = myMouseBindings
---        , layoutHook = layout'
---        , workspaces = myWorkspaces
---        }
-
